6a49fa0fa3f485f360ddf788e4a16b82a190eb6c
Kevin Do <kevin34554@gmail.com>
2021-06-06 17:48:36 -0700

    Consolidated the entire population to a single run
    Also tweaked some small parameters to get the dino to do what we want

--- src/ai_model.py
+++ src/ai_model.py
@@ -15,6 +15,8 @@ from ptera import *
 from cactus import * 
 from game import * 
 
+import pickle
+
 import os
 os.environ["PATH"] += os.pathsep + 'C:\Program Files (x86)\graphviz-0.16\graphviz-0.16\graphviz'
 
@@ -25,66 +27,12 @@ width = 600
 height = 150
 win = pygame.display.set_mode( (width, height) )
 clock = pygame.time.Clock()
-pygame.display.set_caption( "Google Dino Game" )
+pygame.display.set_caption( "Google Chrome Dino Game" )
 
 high_score = 0
 
 FPS = 60
 gravity = 0.6
-
-
-# # 2-input XOR inputs and expected outputs.
-# xor_inputs = [(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0)]
-# xor_outputs = [   (0.0,),     (1.0,),     (1.0,),     (0.0,)]
-
-
-# def eval_genomes(genomes, config):
-#     for genome_id, genome in genomes:
-#         genome.fitness = 4.0
-#         net = neat.nn.FeedForwardNetwork.create(genome, config)
-#         for xi, xo in zip(xor_inputs, xor_outputs):
-#             output = net.activate(xi)
-#             genome.fitness -= (output[0] - xo[0]) ** 2
-
-
-# def run(config_file):
-#     # Load configuration.
-#     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
-#                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
-#                          config_file)
-
-#     # Create the population, which is the top-level object for a NEAT run.
-#     p = neat.Population(config)
-
-#     # Add a stdout reporter to show progress in the terminal.
-#     p.add_reporter(neat.StdOutReporter(True))
-#     stats = neat.StatisticsReporter()
-#     p.add_reporter(stats)
-#     p.add_reporter(neat.Checkpointer(5))
-
-#     # Run for up to 300 generations.
-#     winner = p.run(eval_genomes, 300)
-
-#     # Display the winning genome.
-#     print('\nBest genome:\n{!s}'.format(winner))
-
-#     # Show output of the most fit genome against training data.
-#     print('\nOutput:')
-#     winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
-#     for xi, xo in zip(xor_inputs, xor_outputs):
-#         output = winner_net.activate(xi)
-#         print("input {!r}, expected output {!r}, got {!r}".format(xi, xo, output))
-
-#     node_names = {-1:'A', -2: 'B', 0:'A XOR B'}
-#     visualize.draw_net(config, winner, True, node_names=node_names)
-#     visualize.plot_stats(stats, ylog=False, view=True)
-#     visualize.plot_species(stats, view=True)
-
-#     p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-4')
-#     p.run(eval_genomes, 10)
-
-# dino_inputs = [ "gameSpeed", "yPosition", "distToNextObstable", "typeOfNextObstacle", "heightOfNextObstacle"]
-# dino_outputs = [ "jump", "duck", "unduck", "nothing" ]
 
 gamespeed = 4
 
@@ -106,33 +54,48 @@ def findLargestIndex(output):
 
 
 def ai_plays(genomes, config):
-    # dinos = [Dinosaur(44,47)]*30    # create an array of 30 dinos
-    i = 0
+
+    # Series of lists to contain the genomes and neural networks for each dino in
+    # the population.
+    neuralnetList = []
+    dinos = []
+    genomeList = []
+
     for genome_id, genome in genomes:
         net = neat.nn.FeedForwardNetwork.create(genome, config)
 
-        dino = Dinosaur(44,47)
-        ai_gamer = Game(dino)
-        ai_gamer.run()
-        while not dino.isDead:
-            inputs = [ai_gamer.getGamespeed(), dino.rect.bottom, ai_gamer.getDistToNextObstacle(), ai_gamer.getHeightOfNextObstacle()]
-            output = net.activate(inputs)
+        # initialize the fitness to 0
+        genome.fitness = 0
+
+        # store the genome/neuralnetwork
+        neuralnetList.append(net)
+        dinos.append(Dinosaur(44,47))
+        genomeList.append(genome)
+
+
+    ai_gamer = Game(dinos)
+    ai_gamer.run()
+    while not dinosAreDead( dinos ):
+
+        for i, d in enumerate(dinos):
+            inputs = [ai_gamer.getGamespeed(), d.rect.bottom, ai_gamer.getDistToNextObstacle(), ai_gamer.getHeightOfNextObstacle()]
+            output = neuralnetList[i].activate(inputs) 
+
             index = findLargestIndex(output)
-            # print("index is = " + str(index), flush=True)
             if index == 0:
-                dino.jump()
+                d.jump()
             elif index == 1:
-                dino.duck()
+                d.duck()
             elif index == 2:
-                dino.unduck()
+                d.unduck()
             else:
-                dino.unduck() # do nothing 
+                d.unduck() # do nothing 
+            
+            #subtract by the numbers of times jumped to unincentivize constant jumping
+            genomeList[i].fitness = d.score - d.timesJumped 
 
-            pygame.time.wait(16)  # sleep for 16 milliseconds (1 frame, 60FPS)
-            ai_gamer.updateGame()
-
-        genome.fitness = dino.score
-        # i += 1
+        pygame.time.wait(16)  # sleep for 16 milliseconds (1 frame, 60FPS)
+        ai_gamer.updateGame()
 
 
 def run(config_file):
@@ -156,20 +119,15 @@ def run(config_file):
     # Display the winning genome.
     print('\nBest genome:\n{!s}'.format(winner))
 
-    # Show output of the most fit genome against training data.
-    print('\nOutput:')
-    # winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
-    # for xi, xo in zip(xor_inputs, xor_outputs):
-    #     output = winner_net.activate(xi)
-    #     print("input {!r}, expected output {!r}, got {!r}".format(xi, xo, output))
+    pickle.dump( winner, open( "chad.p", "wb" ) )
 
     node_names = {-1:"gameSpeed", -2:"yPosition", -3:"distToNextObstable", -4:"heightOfNextObstacle", 0:'Jump', 1: 'Duck', 2:'Unduck', 3:"Nothing"}
     visualize.draw_net(config, winner, True, node_names=node_names)
     visualize.plot_stats(stats, ylog=False, view=True)
     visualize.plot_species(stats, view=True)
 
-    #p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-4')
-    #p.run(ai_plays, 1)
+    # p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-4')
+    # p.run(ai_plays, 1)
 
 def main():
     run("./config-feedforward-2.txt")
